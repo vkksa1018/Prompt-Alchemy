4672193ed26a016af27315c548d6244da019f573
james <ohbwgab18@gmail.com>
2026-07-24 11:24:41 +0800

    chore: 移除除錯用的 React Profiler 設定與其渲染日誌

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,12 @@
-import { StrictMode, Profiler } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/index.jsx";
 import "./styles/global.css";
 
-const onRender = (id, phase, actualDuration) => {
-  // 記錄渲染時間，方便在 Console 觀察效能瓶頸
-  console.log(`[React Profiler] ${id} (${phase}) - Render time: ${actualDuration.toFixed(2)}ms`);
-};
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Profiler id="AppRouter" onRender={onRender}>
-      <RouterProvider router={router} />
-    </Profiler>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
 
