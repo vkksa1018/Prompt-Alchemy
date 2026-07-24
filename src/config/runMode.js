415c8dae20813a5e82e2755497601989e8fc26b6
Hughchen2046 <hughchen2046@gmail.com>
2026-07-24 12:36:13 +0800

    調整以線上版優先

--- src/config/runMode.js
+++ src/config/runMode.js
@@ -1,3 +1,3 @@
-export const RUN_MODE = import.meta.env.VITE_RUN_MODE || "local";
+export const RUN_MODE = import.meta.env.VITE_RUN_MODE || "online";
 export const IS_LOCAL_MODE = RUN_MODE === "local";
 export const IS_ONLINE_MODE = RUN_MODE === "online";
